72749d7befabe84957f6c291c480c8dcc8e8c717
mrgoloole <mr.goloole@gmail.com>
2026-06-06 23:46:37 +0330

    add second input

--- main.py
+++ main.py
@@ -12,18 +12,22 @@ load_dotenv()
 running = True
 
 while running:
-    print("Welcome to the Gemini Chat!\n")
 
-    cryptoPrice = get_crypto_price()
+    print("Welcome to the Gemini Chat!\n")
 
     client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-    client_input = input("Ask Gemini a question about analysis current Crypto price: ")
+    symbol_input = input(
+        "Enter the symbol of the cryptocurrency you want to check (e.g., btc, eth): "
+    )
+    client_prompt = input("Ask Gemini a question about analysis current Crypto price: ")
+
+    cryptoPrice = get_crypto_price(symbol_input)
 
     try:
         response = client.models.generate_content(
             model="gemini-2.5-flash",
-            contents=f"{client_input}: {cryptoPrice}",
+            contents=f"{client_prompt}: {cryptoPrice}",
         )
     except (ConnectionError, ValueError, RuntimeError) as e:
         print(f"API Error: {e}")
